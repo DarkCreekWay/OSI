a1ebab892c536b093a6592200ca0fa6fe3bdc86f
DarkCreekWay <47448914+DarkCreekWay@users.noreply.github.com>
2021-08-18 20:01:09 +0200

    Add .NET 5 TFM

--- src/Microsoft/Windows/ComponentObjectModel/Shell/_Library/ShellExtensionManager.cs
+++ src/Microsoft/Windows/ComponentObjectModel/Shell/_Library/ShellExtensionManager.cs
@@ -10,6 +10,9 @@ namespace DarkCreekWay.OSI.Microsoft.Windows.ComponentObjectModel.Shell {
     /// <summary>
     /// Provides functions for managing shell extensions
     /// </summary>
+#if NET5_0_OR_GREATER
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#endif
     public class ShellExtensionManager {
 
         IRegistry _registry;
